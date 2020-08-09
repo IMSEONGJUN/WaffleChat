2eb3e60109f58daad5e3abc3db4440899c20668a
SEONGJUN <naroti@naver.com>
2020-08-09 23:58:18 +0900

    add newMessageVC

--- WaffleChat/WaffleChat/Controller/ConversationsController.swift
+++ WaffleChat/WaffleChat/Controller/ConversationsController.swift
@@ -12,6 +12,14 @@ class ConversationsController: UIViewController {
 
     private let reuseIdentifier = "ConversationCell"
     
+    private let newMessageButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(UIImage(systemName: "plus"), for: .normal)
+        btn.backgroundColor = #colorLiteral(red: 0.6196078431, green: 0.4235294118, blue: 0.1254901961, alpha: 1)
+        btn.tintColor = .white
+        btn.addTarget(self, action: #selector(didTapNewMessageButton), for: .touchUpInside)
+        return btn
+    }()
     
     // MARK: - Properties
     let tableView = UITableView()
@@ -32,6 +40,19 @@ class ConversationsController: UIViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.circle.fill"),
                                                            style: .plain, target: self,
                                                            action: #selector(didTapProfileButton))
+        configureNewMessageButton()
+        
+    }
+    
+    private func configureNewMessageButton() {
+        view.addSubview(newMessageButton)
+        newMessageButton.layer.cornerRadius = 56 / 2
+        newMessageButton.clipsToBounds = true
+        newMessageButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(16)
+            $0.trailing.equalTo(view.snp.trailing).inset(24)
+            $0.width.height.equalTo(56)
+        }
     }
     
     private func configureTableView() {
@@ -73,6 +94,10 @@ class ConversationsController: UIViewController {
             print("Successfully logged out this user")
             switchToLoginVC()
         }
+    }
+    
+    @objc private func didTapNewMessageButton() {
+        print("tap newMessage")
     }
 }
 
---
+++ WaffleChat/WaffleChat/Controller/NewMessageController.swift
@@ -0,0 +1,27 @@
+//
+//  NewMessageController.swift
+//  WaffleChat
+//
+//  Created by SEONGJUN on 2020/08/09.
+//  Copyright © 2020 Seongjun Im. All rights reserved.
+//
+
+import UIKit
+
+
+class NewMessageController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Initial Setup
+    private func configureUI() {
+        
+    }
+    
+}
